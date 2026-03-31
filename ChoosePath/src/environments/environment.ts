4c4fefd3c72ec140677f7ffa2e659e1859a8e49a
andresmacasvelez <167721754+andresmacasvelez@users.noreply.github.com>
2026-03-31 09:15:19 -0500

    Fix Production Settings

--- ChoosePath/src/environments/environment.ts
+++ ChoosePath/src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'http://localhost:4200/api/stories',
   geminiUrl:
     'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent',
-  geminiKey: 'AIzaSyDxUgPx_x0GW6SYhTPXOXrr0ZxhPJad2Pk',
-  mongoDbUrl: 'mongodb://root:90k5V6T(vRQc@144.225.147.44:27017/choosepath?authSource=admin',
+  geminiKey: '',
+  mongoDbUrl: 'mongodb://localhost:27017/choosepath?authSource=admin',
 };
